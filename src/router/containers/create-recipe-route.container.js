55ecb0f5b112305f9b9683fc71987cc50edcec9b
Mikhail_Podkhvatilin <podkhvatilin_my@groupbwt.com>
2021-07-08 12:55:30 +0300

    change route rules

--- src/router/containers/create-recipe-route.container.js
+++ src/router/containers/create-recipe-route.container.js
@@ -1,18 +1,15 @@
-//import { useSelector } from 'react-redux'
-//import { Redirect } from 'react-router-dom'
-//import { selectIsAuthenticatedUser } from '../../redux/user/user.selectors'
+import { useSelector } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+import { selectIsAuthenticatedUser } from '../../redux/user/user.selectors'
 import CreateRecipePage from '../../pages/create-recipe-page/create-recipe-page.component'
 
 const CreateRecipeRouteContainer = () => {
-  //TODO remove after develop
-  //const isAuthenticatedUser = useSelector(selectIsAuthenticatedUser)
-  //
-  // return isAuthenticatedUser ?
-  //   <CreateRecipePage />
-  //   :
-  //   <Redirect to='/' />
+  const isAuthenticatedUser = useSelector(selectIsAuthenticatedUser)
 
-  return <CreateRecipePage />
+  return isAuthenticatedUser ?
+    <CreateRecipePage />
+    :
+    <Redirect to='/' />
 }
 
 export default CreateRecipeRouteContainer
