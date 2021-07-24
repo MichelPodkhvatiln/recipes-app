8f8a426fa7908210108a7857ab490d3d080fbc5f
Mikhail_Podkhvatilin <podkhvatilin_my@groupbwt.com>
2021-07-24 12:11:07 +0300

    fix dev tools in prod

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,6 +5,7 @@ import { persistedReducer } from './root.reducer'
 
 export const store = configureStore({
   reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
